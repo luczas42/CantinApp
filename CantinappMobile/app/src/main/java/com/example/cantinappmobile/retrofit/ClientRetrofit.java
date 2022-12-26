ecf159028fac535e2c2c119458622d9902124b6d
Neiscafe <110692081+Neiscafe@users.noreply.github.com>
2022-12-26 09:36:34 -0300

    Adição de logging interceptor ao cliente retrofit

--- CantinappMobile/app/src/main/java/com/example/cantinappmobile/retrofit/ClientRetrofit.java
+++ CantinappMobile/app/src/main/java/com/example/cantinappmobile/retrofit/ClientRetrofit.java
@@ -1,5 +1,7 @@
 package com.example.cantinappmobile.retrofit;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,11 +11,16 @@ public class ClientRetrofit {
 
     public static Retrofit getInstance() {
 
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
         if (retrofit == null) {
             final String urlWebService = "http://54.94.3.48/testephp/";
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(urlWebService)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
