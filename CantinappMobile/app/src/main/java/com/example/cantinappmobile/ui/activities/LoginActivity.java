c51c5ff09785d34a4fc77dcf1c2c11d086e7b670
Neiscafe <110692081+Neiscafe@users.noreply.github.com>
2023-02-01 11:26:43 -0300

    tela de login aparecendo no design

--- CantinappMobile/app/src/main/java/com/example/cantinappmobile/ui/activities/LoginActivity.java
+++ CantinappMobile/app/src/main/java/com/example/cantinappmobile/ui/activities/LoginActivity.java
@@ -1,16 +1,21 @@
 package com.example.cantinappmobile.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 import com.example.cantinappmobile.R;
+import com.example.cantinappmobile.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
