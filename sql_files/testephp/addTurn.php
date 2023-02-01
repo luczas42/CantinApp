05f4acfa9d43e4ff5c85f796ef7f4b243320e7d9
Neiscafe <110692081+Neiscafe@users.noreply.github.com>
2023-02-01 19:47:47 -0300

    todos os cruds de turn

---
+++ sql_files/testephp/addTurn.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json charset=utf-8');
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    include 'setupConnection.php';
+
+    if(!empty($_POST['day']) && !empty($_POST['period']) 
+    && !empty($_POST['class'])){
+
+        $day = $_POST['day'];
+        $period = $_POST['period'];
+        $class = $_POST['class'];
+
+        $sql = "insert into turn (day, period, class) values (?, ?, ?);";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('sis', $day, $period, $class);
+        $stmt->execute();
+    }else{
+    }
+    $conn->close();
+}
+
+
+?>
