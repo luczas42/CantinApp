15bd27c8504c84a56c997218560f06c381aec042
Neiscafe <110692081+Neiscafe@users.noreply.github.com>
2023-02-09 11:19:39 -0300

    mudanças ☺☺☺

--- CantinappServer/dbConnection.php
+++ CantinappServer/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
-    $HostName = "localhost";
-    $HostUser = "root";
-    $HostPass = "";
-    $DatabaseName = "cantinapp";
+    $HostName = "sql303.epizy.com";
+    $HostUser = "epiz_33557193";
+    $HostPass = "neJhT2XG5K";
+    $DatabaseName = "epiz_33557193_cantinapp";
 ?>
